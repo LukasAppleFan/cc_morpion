ee81a5a7896dcd86a089df620403e80777710c61
LukasAppleFan <41242166+LukasAppleFan@users.noreply.github.com>
2020-04-16 14:22:34 +0200

    Essaie

--- PythonMorpion.py
+++ PythonMorpion.py
@@ -1,3 +1,21 @@
-print("['x', ' ', 'o']")
-print("[' ', ' ', ' ']")
-print("['x', ' ', ' ']")
+import pygame
+import sys
+
+joueur1 = 1
+joueur2 = 2
+
+tab = [[' ', ' ', ' '],
+		[' ', ' ', ' '],
+		[' ', ' ', ' '],]
+
+for row in tab:
+	print(row)
+
+tab[0][0] = 'x'
+
+for row in tab:
+	print(row)
+
+value = input("Entrer en x :\n")
+
+print(f'You entered {value}')
