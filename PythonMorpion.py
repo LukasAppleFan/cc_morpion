8ea87ace8857dfaf6bbfb9d97f1ad29e6e77ba6b
LukasAppleFan <41242166+LukasAppleFan@users.noreply.github.com>
2020-04-16 14:41:19 +0200

    Avance

--- PythonMorpion.py
+++ PythonMorpion.py
@@ -1,21 +1,56 @@
 import pygame
 import sys
 
-joueur1 = 1
-joueur2 = 2
+# Variables
 
-tab = [[' ', ' ', ' '],
-		[' ', ' ', ' '],
-		[' ', ' ', ' '],]
+board = ["-", "-", "-",
+		"-", "-", "-",
+		"-", "-", "-"]
 
-for row in tab:
-	print(row)
+game_still_going = True
 
-tab[0][0] = 'x'
+winner = None
 
-for row in tab:
-	print(row)
+current_player = "X"
 
-value = input("Entrer en x :\n")
+def display_board():
+	print(board[0] + " | " + board[1] + " | " + board[2])
+	print(board[3] + " | " + board[4] + " | " + board[5])
+	print(board[6] + " | " + board[7] + " | " + board[8])
 
-print(f'You entered {value}')
+def play_game():
+
+	#Affiche le tableau initial
+	display_board()
+
+	while game_still_going:
+
+		handle_turn(current_player)
+
+		check_if_game_over()
+
+		flip_player()
+
+
+def handle_turn(player):
+	position = input("Choisis une position de 1 à 9 :\n")
+	position = int(position) - 1
+
+	board[position] = "X"
+	display_board()
+
+def check_if_game_over():
+	check_if_win()
+	check_if_tie()
+
+def check_if_win():
+
+	return
+
+def check_if_tie():
+	return
+
+def flip_player():
+	return
+
+play_game()
